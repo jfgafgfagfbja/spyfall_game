03579d85b3056c4c692858aa6b91c005a3a03ed7
jfgafgfagfbja <165975411+jfgafgfagfbja@users.noreply.github.com>
2026-03-10 16:02:34 +0700

    chore: add firebase web config

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 window.SPYFALL_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDLIhhmKBsKK74bp_HSVmoo43HvnvDD6oI",
+  authDomain: "spyfall-game-4d753.firebaseapp.com",
+  databaseURL: "https://spyfall-game-4d753-default-rtdb.firebaseio.com",
+  projectId: "spyfall-game-4d753",
+  storageBucket: "spyfall-game-4d753.firebasestorage.app",
+  messagingSenderId: "839830138002",
+  appId: "1:839830138002:web:a86d460f6329c16ce98a4b"
 };
